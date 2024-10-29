3573bc0a78a3e3a3f6592b7dc8f25e8d8d0d1005
Basti <89860334+Xerolux@users.noreply.github.com>
2024-10-29 13:05:51 +0100

    Update if.go

--- pkg/message/if.go
+++ pkg/message/if.go
@@ -2,23 +2,30 @@ package message
 
 import "io"
 
-// Reader read message for specified application protocol from client and target server
+// Reader defines an interface for reading messages for specific application protocols
 type Reader interface {
-	ReadMessage(conn io.Reader) ([]byte, error)
-	Name() string
+	ReadMessage(conn io.Reader) ([]byte, error) // Reads a message from the connection
+	Name() string                               // Returns the name of the protocol
 }
 
+// Readers is a map of available protocol readers, keyed by protocol name
 var Readers map[string]Reader
 
+// init initializes the Readers map and registers all available protocol readers
 func init() {
 	Readers = make(map[string]Reader)
-	for _, msgReader := range []Reader{
+	registerReaders(
 		&EchoMessageReader{},
 		&HTTPMessageReader{},
 		&ISO8583MessageReader{},
 		&MPUMessageReader{},
 		&ModbusMessageReader{},
-	} {
+	)
+}
+
+// registerReaders adds protocol readers to the Readers map
+func registerReaders(readers ...Reader) {
+	for _, msgReader := range readers {
 		Readers[msgReader.Name()] = msgReader
 	}
 }
