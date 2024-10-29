95e3294012b572087cbab049f89415d24b937080
Basti <89860334+Xerolux@users.noreply.github.com>
2024-10-29 13:08:25 +0100

    Update echo.go

--- pkg/message/echo.go
+++ pkg/message/echo.go
@@ -5,16 +5,25 @@ import (
 	"io"
 )
 
-// https://tools.ietf.org/html/rfc862
+// EchoMessageReader is a reader for the Echo protocol as defined in RFC 862.
+// It reads messages that are newline ('\n') terminated.
+type EchoMessageReader struct{}
 
-type EchoMessageReader struct {
-}
-
+// Name returns the name of the protocol for this reader.
 func (e EchoMessageReader) Name() string {
 	return "echo"
 }
 
-// ReadMessage message is expected \n terminated
+// ReadMessage reads a message from the connection until a newline ('\n') is encountered.
+// It returns the message as a byte slice or an error if reading fails.
 func (e EchoMessageReader) ReadMessage(conn io.Reader) ([]byte, error) {
-	return bufio.NewReader(conn).ReadBytes('\n')
+	reader := bufio.NewReader(conn)
+	
+	// Read bytes until a newline ('\n') character is encountered
+	message, err := reader.ReadBytes('\n')
+	if err != nil {
+		return nil, err // Return error if reading fails
+	}
+	
+	return message, nil // Return the message as a byte slice
 }
